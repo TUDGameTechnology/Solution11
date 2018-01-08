44f1c807dc7736a8618ad25f7db42fbb537f957a
Polona Caserman <polona.caserman@gmail.com>
2018-01-08 11:58:32 +0100

    Update Kore

---
+++ Sources/MeshObject.h
@@ -0,0 +1,13 @@
+//
+//  MeshObject.h
+//  Exercise11
+//
+//  Created by Polona Caserman on 08.01.18.
+//  Copyright © 2018 KTX Software Development. All rights reserved.
+//
+
+#ifndef MeshObject_h
+#define MeshObject_h
+
+
+#endif /* MeshObject_h */
---
+++ Sources/pch.h
@@ -0,0 +1,13 @@
+//
+//  pch.h
+//  Exercise11
+//
+//  Created by Polona Caserman on 08.01.18.
+//  Copyright © 2018 KTX Software Development. All rights reserved.
+//
+
+#ifndef pch_h
+#define pch_h
+
+
+#endif /* pch_h */
